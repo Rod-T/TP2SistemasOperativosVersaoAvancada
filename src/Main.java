a89c4991bfe2b2f9b6d4ac26057156eff17bbbf5
Rod <201701425@estudantes.ips.pt>
2022-02-08 18:03:16 +0000

    fixed heap mem bug

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,17 @@ public class Main {
     public static void main(String[] args) {
         /*String file = args[0];
         int threads = Integer.parseInt(args[1]);
-        int maxTime = Integer.parseInt(args[2]);*/
+        int maxTime = Integer.parseInt(args[2]);
+        int percentage = Integer.parseInt(args[3]);
+        double timeToSleep = (double)percentage / 100;
+        int percIterations = 100 / percentage;*/
 
         String file = "prob08_3.txt";
-        int threads = 20;
+        int threads = 4;
         int maxTime = 2000;
+        int percentage = 50;
+        double percentageTimeToSleep = (double)percentage / 100;
+        int percentageIterations = 100 / percentage;
 
         AllPopulation bestPopulation = new AllPopulation(threads);
 
@@ -39,7 +45,37 @@ public class Main {
                     myThreads.get(j).start();
                 }
 
-                Thread.sleep((long)(maxTime*0.5));
+                for (int k = 0 ; k < percentageIterations ; k++ ){
+                    for (int j = 0; j < threads; j++) {
+                        myThreads.get(j).setFlag(0);
+                    }
+                    Thread.sleep((long)(maxTime*percentageTimeToSleep));
+
+                    for (int j = 0; j < threads; j++) {
+                        myThreads.get(j).interrupt();
+                    }
+
+                    bestPopulation.getBest75();
+                    bestPopulation.allPopulation.clear();
+
+                    for (int j = 0; j < threads; j++) {
+                        myThreads.get(j).getBestPopulation();
+                    }
+
+                    for (int j = 0; j < threads; j++) {
+                        myThreads.get(j).setFlag(1);
+                    }
+
+                    if(k == percentageIterations - 1){
+                        Thread.sleep((long)(maxTime*percentageTimeToSleep));
+                        for (int j = 0; j < threads; j++) {
+                            myThreads.get(j).setFlag(2);
+                        }
+                    }
+                }
+
+
+                /*Thread.sleep((long)(maxTime*percentageTimeToSleep));
 
                 for (int j = 0; j < threads; j++) {
                     myThreads.get(j).interrupt();
@@ -54,13 +90,13 @@ public class Main {
 
                 for (int j = 0; j < threads; j++) {
                     myThreads.get(j).setFlag(1);
-                }
+                }*/
 
-                Thread.sleep((long)(maxTime*0.5));
+                //Thread.sleep((long)(maxTime*percentageTimeToSleep));
 
-                for (int j = 0; j < threads; j++) {
+                /*for (int j = 0; j < threads; j++) {
                     myThreads.get(j).setFlag(2);
-                }
+                }*/
 
                 for (int j = 0; j < threads; j++) {
                     myThreads.get(j).join();
